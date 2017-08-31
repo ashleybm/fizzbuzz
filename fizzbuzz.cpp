a6f6c0e71d9be4be874a00495c15e0991dbf8587
Ashley Baker <ashleybm@umich.edu>
2017-08-30 21:48:58 -0400

    Push Fizzbuzz

---
+++ fizzbuzz.cpp
@@ -0,0 +1,24 @@
+/*
+Ashley Baker
+Implementation of fizzbuzz
+Description: Prints Numbers 1-100 unless they_
+are multiples of 3 or 5. If multiple of 3 print_
+fizz, if multiple of 5 print buzz, if multiple_
+of both, print fizzbuzz.*/
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+	for (int i = 0; i < 100; i++) {
+		if (i % 3 == 0)
+			cout << "fizz";
+		else if (i % 5 == 0)
+			cout << "buzz";
+		else
+			cout << i;
+	}
+
+}
